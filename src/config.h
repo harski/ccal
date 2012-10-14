08629ce407775cafa96a776aa0fb017b2ce2f326
Tuomo Hartikainen <hartitu@gmail.com>
2012-10-14 23:53:44 +0300

    Add debug_print() to config.h

--- src/config.h
+++ src/config.h
@@ -3,6 +3,12 @@
 
 #define _POSIX_C_SOURCE 200809L
 
+#define DEBUG 1
+
+#define debug_print(fmt, ...) \
+        do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
+                                __LINE__, __func__, __VA_ARGS__); } while (0)
+
 
 #endif /* CONFIG_H */
 
