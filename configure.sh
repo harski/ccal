109603ea544bc8f8f21ecdfa61db46fd7624f2aa
Tuomo Hartikainen <hartitu@gmail.com>
2012-10-31 14:04:40 +0200

    Dynamically generate src/config.h
    Delete the static src/config.h. It is now generated with configure.sh
    script. src/VERSION is now mandatory.

---
+++ configure.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+srcdir="src"
+VERSION=$(cat "${srcdir}/VERSION")
+CONFIG_HEADER="${srcdir}/config.h"
+DEBUG=0
+
+function addtoconfig {
+    echo "$1" >> $CONFIG_HEADER
+}
+
+alias atc='addtoconfig'
+
+function write_config_h {
+# If $CONFIG_HEADER exists, empty it
+    if [ -f $CONFIG_HEADER ] ;then
+        rm $CONFIG_HEADER
+        touch $CONFIG_HEADER
+    fi
+
+    atc "#ifndef CONFIG_H"
+    atc "#define CONFIG_H"
+    atc ""
+
+    atc "#define _POSIX_C_SOURCE 200809L"
+    atc ""
+
+    atc "#define VERSION \"$VERSION\""
+    atc "#define DEBUG $DEBUG"
+    atc ""
+
+    atc ""
+    atc "#endif /* CONFIG_H */"
+    atc ""
+}
+
+write_config_h
