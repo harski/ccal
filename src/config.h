3b7b585ea51627961f3506f72cbcc6742348ec09
Tuomo Hartikainen <hartitu@gmail.com>
2012-10-31 13:53:38 +0200

    Move debug_print from config.h to debug.h

--- src/config.h
+++ src/config.h
@@ -8,10 +8,5 @@
 
 #define DEBUG 1
 
-#define debug_print(fmt, ...) \
-        do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
-                                __LINE__, __func__, __VA_ARGS__); } while (0)
-
-
 #endif /* CONFIG_H */
 
---
+++ src/debug.h
@@ -0,0 +1,9 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#define debug_print(fmt, ...) \
+        do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
+                                __LINE__, __func__, __VA_ARGS__); } while (0)
+
+#endif /* DEBUG_H */
+
