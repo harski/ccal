a3953b8994592c522666ce5664f009b7d8b57f3f
Tuomo Hartikainen <hartitu@gmail.com>
2013-01-14 22:54:40 +0200

    Comment functions on timeutils.h

--- src/timeutils.h
+++ src/timeutils.h
@@ -10,8 +10,15 @@
 
 /* If tm is NULL it is allocated. Returns NULL on error */
 struct tm *get_today(struct tm *tm);
+
+/* Advance tm 24 hours */
 void next_day (struct tm *tm);
+
+/* Decrease tm 24 hours */
 void prev_day (struct tm *tm);
+
+/* returns true if tm1 and tm2 are the same day
+ * (NOTE! Not the same time) */
 bool same_day (const struct tm *tm1, const struct tm *tm2);
 
 #endif /* TIMEUTILS_H */
